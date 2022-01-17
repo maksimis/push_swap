8bf8ac4bb1e3d78c4e81da28beab08f6f2bf6396
= <=>
2022-01-17 22:40:57 +0300

    push swap

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
-int main(void)
+int main(int argc, char **argv)
 {
-	write(1, "hello world", 11);
+	int	i;
+
+	if (argc == 1)
+		return (0);
+	i = 1;
+	while (i <= argc)
+	{
+		write(1, argv[i], 2);
+		write(1, "\n", 1);
+		i++;
+	}
+	
+	write(1, "hello world\n", 12);
 }
