b391a2c27b3cb40013e4c193b064d3674b99e304
cmichael <cmichael@student.21-school.ru>
2022-01-23 13:49:45 +0300

    push swap args

--- main.c
+++ main.c
@@ -3,15 +3,35 @@
 int main(int argc, char **argv)
 {
 	int	i;
+	char *all_arg;
+	char *tmp;
+	char *tmp2;
+	char **splited_args;
 
 	if (argc == 1)
 		return (0);
 	i = 1;
-	while (i <= argc)
+	all_arg = ft_strdup("");
+	while (i < argc)
 	{
-		ft_putendl_fd(argv[i], 1);
+		if (ft_strlen(all_arg) > 0)
+		{
+			tmp2 = all_arg;
+			all_arg = ft_strjoin(all_arg, " ");
+			free(tmp2);
+		}
+		tmp = all_arg;
+		all_arg = ft_strjoin(all_arg, argv[i]);
+		free(tmp);
 		i++;
 	}
-	
-	write(1, "hello world\n", 12);
+	splited_args = ft_split(all_arg, ' ');
+	free(all_arg);
+	while (*splited_args)
+	{
+		ft_putendl_fd(*splited_args, 1);
+		free(*splited_args);
+		splited_args++;
+	}
+	return (1);
 }
